004109e868e158bb1e95cf0bcd36cada78351149
Katsuya-Ishiyama <ishiyama.katsuya@gmail.com>
2022-01-03 23:53:12 +0900

    MySQLからSQLite3に変更

--- data/db/__init__.py
+++ data/db/__init__.py
@@ -1,3 +1,3 @@
 from sqlalchemy import create_engine
 
-engine = create_engine("mysql://root:root@127.0.0.1:3306/testdb?charset=utf8")
+engine = create_engine("sqlite:///data/db/sqlite3/jisan-db.sqlite3")
